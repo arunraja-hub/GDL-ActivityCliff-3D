4e8a49ec03ae2522e340b830896187053048e142
arunraja-hub <surviva614@gmail.com>
2023-05-10 11:18:40 +0100

    e3fp

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,7 +3,7 @@ from .molecular_processing import *
 from .deep_learning_pytorch import *
 from .scoring import *
 from .visualisation import *
-# from .gin import *
+from .gin import *
 # from .gcn import *
 # from .gsn import *
 from .helper import *
