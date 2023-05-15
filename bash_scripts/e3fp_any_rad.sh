053e664ec56917850bc01125a4885e2f5a4d8d6c
arunraja-hub <surviva614@gmail.com>
2023-05-15 11:49:57 +0100

    bash

--- bash_scripts/e3fp_any_rad.sh
+++ bash_scripts/e3fp_any_rad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash   
-#SBATCH -J gcn_mlp                     # Job name
-#SBATCH --time=12:00:00                 # Walltime                                      
+#SBATCH -J e3fp05                     # Job name
+#SBATCH --time=48:00:00                 # Walltime                                      
 #SBATCH --mem-per-cpu=16G             # memory/cpu (in MB) ### commented out              
 #SBATCH --ntasks=1                      # 1 tasks                                               
 #SBATCH --cpus-per-task=1           # number of cores per task                          
@@ -14,6 +14,7 @@
 #SBATCH --mail-type=end                 # ... and ends
 #S BATCH --partition=nagagpu04-high-debug  # Select a specific partition rather than default 
 #SBATCH --clusters=all
+#S BATCH --partition=high-opig-test    # Select a specific partition rather than default
 #S BATCH -w nagagpu04.cpu.stats.ox.ac.uk # Provide a specific node/nodelist rather than the standard nodelist associated with the partition (useful if you have a data setup on one specific node)
 #SBATCH --output=/vols/opig/users/raja/slurm_outs/slurm_%j.out  # Writes standard output to this file. %j is jobnumber                             
 #SBATCH --error=/vols/opig/users/raja/slurm_outs/slurm_%j.err   # Writes error messages to this file. %j is jobnumber
@@ -25,9 +26,15 @@ source /vols/opig/users/raja/miniconda3/etc/profile.d/conda.sh
 conda activate gin_conda
 echo "dataset: $1";
 echo "model: $2";
-#pip3 install  pandas torch torchvision torchaudio matplotlib numpy rdkit scikit_learn scipy seaborn spektral statsmodels tensorflow torch_geometric e3fp networkx optuna --cache-dir /vols/opig/users/raja/cache
-# -t /vols/opig/users/raja
-python GDL-ActivityCliff-3D/gcn_exp.py --dataset $1 --model $2 >> GDL-ActivityCliff-3D/terminal_output/gcn_$1_$2.txt
-#python GDL-ActivityCliff-3D/gin_exp.py --dataset postera_sars_cov_2_mpro --model rf >> GDL-ActivityCliff-3D/terminal_output/gin_rf.txt
-#python GDL-ActivityCliff-3D/gin_exp.py --dataset postera_sars_cov_2_mpro --model knn >> GDL-ActivityCliff-3D/terminal_output/gin_knn2.txt
-#python GDL-ActivityCliff-3D/gin_exp.py --dataset postera_sars_cov_2_mpro --model mlp >> GDL-ActivityCliff-3D/terminal_output/gin_mlp.txt
+echo "rad: $3";
+python GDL-ActivityCliff-3D/e3fp_exp.py --dataset $1 --model $2 --rad $3 
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset chembl_factor_xa --model rf --rad 0.5
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset postera_sars_cov_2_mpro --model rf --rad 0.5
+# echo "all proteins rad 0.5 - knn"
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset chembl_factor_xa --model knn --rad 0.5
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset chembl_dopamine_d2 --model knn --rad 0.5
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset postera_sars_cov_2_mpro --model knn --rad 0.5
+# echo "all proteins rad 0.5 - mlp"
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset chembl_factor_xa --model mlp --rad 0.5
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset chembl_dopamine_d2 --model mlp --rad 0.5
+# python GDL-ActivityCliff-3D/e3fp_exp.py --dataset postera_sars_cov_2_mpro --model mlp --rad 0.5
