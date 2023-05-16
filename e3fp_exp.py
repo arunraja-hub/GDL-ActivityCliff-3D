34de1a6e690052b522e082fb1fb017b34eb2325b
arunraja-hub <surviva614@gmail.com>
2023-05-16 14:28:15 +0100

    e3fp

--- e3fp_exp.py
+++ e3fp_exp.py
@@ -93,12 +93,13 @@ if len(x_smiles) == len(x_smiles_to_fp_dict):
 
 else:
     print("len(x_smiles) == len(x_smiles_to_fp_dict)",len(x_smiles), len(x_smiles_to_fp_dict))
-    # print('generating e3fp smiles for radius multiplier =', args.rad)
-    # for smiles in x_smiles:
-    #     if smiles not in x_smiles_to_fp_dict:
-    #         x_smiles_to_fp_dict.update({smiles : e3fp_from_smiles(smiles, float(args.rad))})
-    #         with open(datafolder_filepath +'/rad'+args.rad+'_smiles_e3fp_dict.pkl', 'wb') as f:
-    #             pickle.dump(x_smiles_to_fp_dict,f)
+    print('generating e3fp smiles for radius multiplier =', args.rad)
+    for smiles in x_smiles:
+        if smiles not in x_smiles_to_fp_dict:
+            print(smiles)
+            # x_smiles_to_fp_dict.update({smiles : e3fp_from_smiles(smiles, float(args.rad))})
+            # with open(datafolder_filepath +'/rad'+args.rad+'_smiles_e3fp_dict.pkl', 'wb') as f:
+            #     pickle.dump(x_smiles_to_fp_dict,f)
         # break
     print('DONE---generating e3fp smiles for radius multiplier =', args.rad)
 
