02b16c37faf1de5f339632d2f1c302bfa0537b6f
arunraja-hub <surviva614@gmail.com>
2023-05-24 22:58:19 +0100

    svgtopdf.py

---
+++ svgtopdf.py
@@ -0,0 +1,10 @@
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPDF
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', type=str, help='Description of arg1')
+args = parser.parse_args()
+
+
+drawing = svg2rlg(args.file)
+renderPDF.drawToFile(drawing, args = parser.parse_args()[:-4]+".pdf")
