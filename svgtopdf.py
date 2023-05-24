1eac4b618300a77c863298ee228987c3510e8d8b
arunraja-hub <surviva614@gmail.com>
2023-05-24 23:01:14 +0100

    svgtopdf.py

--- svgtopdf.py
+++ svgtopdf.py
@@ -6,5 +6,5 @@ parser.add_argument('--file', type=str, help='Description of arg1')
 args = parser.parse_args()
 
 
-drawing = svg2rlg(args.file)
+drawing = svg2rlg(str(args.file))
 renderPDF.drawToFile(drawing, args = parser.parse_args()[:-4]+".pdf")
